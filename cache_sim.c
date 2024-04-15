cbc1d3dcb1c6c4f6f4bdc6685c38e1201c3a3d7d
BlankTempest <61834898+BlankTempest@users.noreply.github.com>
2024-04-16 01:28:30 +0530

    bus

--- cache_sim.c
+++ cache_sim.c
@@ -14,21 +14,18 @@ typedef char byte;
 #define EXCLUSIVE 2
 #define SHARED 3
 
-struct cache {
+typedef struct {
     byte address; // This is the address in memory.
     byte value; // This is the value stored in cached memory.
     // State for you to implement MESI protocol.
     byte state;
-};
+} cache_line;
 
-struct decoded_inst {
+typedef struct {
     int type; // 0 is RD, 1 is WR
     byte address;
     byte value; // Only used for WR 
-};
-
-typedef struct cache cache_line;
-typedef struct decoded_inst decoded;
+} decoded_inst;
 
 /*
 * This is a very basic C cache simulator.
@@ -39,195 +36,183 @@ typedef struct decoded_inst decoded;
 */
 
 byte memory[MEMORY_SIZE];
-cache_line cache[CACHE_SIZE];
+cache_line **caches; //pointer
 
-void initialize_memory() {
-    for (int i = 0; i < MEMORY_SIZE; i++) {
-        memory[i] = 0;
-    }
-}
-
-void initialize_cache() {
-    for (int i = 0; i < CACHE_SIZE; i++) {
-        cache[i].address = -1;
-        cache[i].value = 0;
-        cache[i].state = INVALID;
-    }
-}
+omp_lock_t *bus_locks;
 
 // Decode instruction lines
-decoded decoded_inst_line(char * buffer){
-    decoded inst;
-    char inst_type[2];
-    sscanf(buffer, "%s", inst_type);
-    if(!strcmp(inst_type, "RD")){
+decoded_inst decode_inst_line(char *buffer) {
+    decoded_inst inst;
+    char inst_type[3];
+    int addr, val = 0;
+    sscanf(buffer, "%s %d %d", inst_type, &addr, &val);
+    
+    if (strcmp(inst_type, "RD") == 0) {
         inst.type = 0;
-        int addr = 0;
-        sscanf(buffer, "%s %d", inst_type, &addr);
+        inst.address = (byte)addr;
         inst.value = -1;
-        inst.address = addr;
-    } else if(!strcmp(inst_type, "WR")){
+    } else if (strcmp(inst_type, "WR") == 0) {
         inst.type = 1;
-        int addr = 0;
-        int val = 0;
-        sscanf(buffer, "%s %d %d", inst_type, &addr, &val);
-        inst.address = addr;
-        inst.value = val;
+        inst.address = (byte)addr;
+        inst.value = (byte)val;
     }
     return inst;
 }
 
-void decode_inst_line(char *buffer, char *inst_type, int *addr, int *val) {
-    if (sscanf(buffer, "%s %d %d", inst_type, addr, val) != 3) {
-        sscanf(buffer, "%s %d", inst_type, addr);
-        *val = 0;
-    }
-}
-
-int coherence_check(int addr) {
+int cache_lookup(cache_line *cache, byte address) {
     for (int i = 0; i < CACHE_SIZE; i++) {
-        if (cache[i].address == addr) {
+        if (cache[i].address == address) {
             return i;
         }
     }
     return -1;
 }
 
-void update_cache(int idx, int addr, int val, int inst_type) {
-    #pragma omp critical
-    {
-        for (int i = 0; i < CACHE_SIZE; i++) {
-            if (cache[i].address == addr && i != idx) {
-                cache[i].state = INVALID;
-            }
-        }
-
-        cache[idx].address = addr;
-        cache[idx].value = val;
-
-        if (inst_type == 0) { // read
-            cache[idx].state = SHARED;
-        } else { // write
-            if (cache[idx].state == INVALID) {
-                cache[idx].state = MODIFIED;
-            } else {
-                cache[idx].state = MODIFIED;
-            }
-        }
-    }
+void update_cache_line(cache_line *cache, int cache_idx, byte address, byte value, byte state) {
+    cache[cache_idx].address = address;
+    cache[cache_idx].value = value;
+    cache[cache_idx].state = state;
 }
 
-void write_back_to_memory(int idx) {
-    #pragma omp critical
-    {
-        if (cache[idx].state == MODIFIED) {
-            memory[cache[idx].address] = cache[idx].value;
-            cache[idx].state = SHARED;
+void handle_cache_hit(int thread_num, int cache_idx, decoded_inst inst) {
+    cache_line *cache = caches[thread_num];
+    
+    if (cache[cache_idx].state == MODIFIED) {
+        if (inst.type == 1) {
+            cache[cache_idx].value = inst.value;
         }
-    }
-}
+        printf("Thread %d: %s %d: %d (cache hit, modified state)\n",
+               thread_num, inst.type == 0 ? "RD" : "WR", inst.address, cache[cache_idx].value);
+    } else if (cache[cache_idx].state == SHARED) {
+        if (inst.type == 1) {
+            omp_set_lock(&bus_locks[inst.address]);
+            
+            for (int i = 0; i < omp_get_num_procs(); i++) {
+                if (i != thread_num) {
+                    int other_cache_idx = cache_lookup(caches[i], inst.address);
+                    if (other_cache_idx != -1) {
+                        caches[i][other_cache_idx].state = INVALID;
+                    }
+                }
+            }
+            cache[cache_idx].state = MODIFIED;
+            cache[cache_idx].value = inst.value;
 
-// Helper function to print the cachelines
-void print_cachelines(cache_line * c, int cache_size){
-    for(int i = 0; i < cache_size; i++){
-        cache_line cacheline = *(c+i);
-        printf("Address: %d, State: %d, Value: %d\n", cacheline.address, cacheline.state, cacheline.value);
+            omp_unset_lock(&bus_locks[inst.address]);
+
+            printf("Thread %d: WR %d: %d (cache hit, shared state)\n",
+                   thread_num, inst.address, inst.value);
+        } else {
+            printf("Thread %d: RD %d: %d (cache hit, shared state)\n",
+                   thread_num, inst.address, cache[cache_idx].value);
+        }
+    } else {
+        if (inst.type == 0) {
+            printf("Thread %d: RD %d: %d (cache hit, exclusive state)\n",
+                   thread_num, inst.address, cache[cache_idx].value);
+        } else {
+            cache[cache_idx].value = inst.value;
+            cache[cache_idx].state = MODIFIED;
+            printf("Thread %d: WR %d: %d (cache hit, exclusive state)\n",
+                   thread_num, inst.address, inst.value);
+        }
     }
 }
 
 // This function implements the mock CPU loop that reads and writes data.
-void cpu_loop(int num_threads){
-    // Initialize a CPU level cache that holds about 2 bytes of data.
-    int cache_size = 2;
-    cache_line * c = (cache_line *) malloc(sizeof(cache) * cache_size);
-    
-    // Read Input file
-    FILE * inst_file = fopen("input_0.txt", "r");
-    char inst_line[20];
-    // Decode instructions and execute them.
-    while (fgets(inst_line, sizeof(inst_line), inst_file)){
-        decoded inst = decoded_inst_line(inst_line);
-        /*
-         * Cache Replacement Algorithm
-         */
-        int hash = inst.address%cache_size;
-        cache_line cacheline = *(c+hash);
-        /*
-         * This is where you will implement the coherancy check.
-         * For now, we will simply grab the latest data from memory.
-         */
-        if(cacheline.address != inst.address){
-            // Flush current cacheline to memory
-            *(memory + cacheline.address) = cacheline.value;
-            // Assign new cacheline
-            cacheline.address = inst.address;
-            cacheline.state = -1;
-            // This is where it reads value of the address from memory
-            cacheline.value = *(memory + inst.address);
-            if(inst.type == 1){
-                cacheline.value = inst.value;
-            }
-            *(c+hash) = cacheline;
-        }
-        switch(inst.type){
-            case 0:
-                printf("Reading from address %d: %d\n", cacheline.address, cacheline.value);
-                break;
-            
-            case 1:
-                printf("Writing to address %d: %d\n", cacheline.address, cacheline.value);
-                break;
-        }
-    }
-    free(c);
-}
-
 void cpu_thread(int thread_num) {
     char filename[15];
     sprintf(filename, "input_%d.txt", thread_num);
     FILE *inst_file = fopen(filename, "r");
+    
+    if (!inst_file) {
+        perror("Failed to open input file");
+        return;
+    }
+    
+    cache_line *cache = caches[thread_num];
     char inst_line[20];
+    
     while (fgets(inst_line, sizeof(inst_line), inst_file)) {
-        char inst_type[3];
-        int addr, val;
-        decode_inst_line(inst_line, inst_type, &addr, &val);
-        int cache_idx = coherence_check(addr);
-        switch (inst_type[0]) {
-            case 'R':
-                if (cache_idx == -1) {
-                    printf("Thread %d: RD %d: %d\n", thread_num, addr, memory[addr]);
-                    update_cache(thread_num % CACHE_SIZE, addr, memory[addr], 0);
-                } else {
-                    write_back_to_memory(cache_idx);
-                    printf("Thread %d: RD %d: %d\n", thread_num, addr, cache[cache_idx].value);
+        decoded_inst inst = decode_inst_line(inst_line);
+        int cache_idx = cache_lookup(cache, inst.address);
+        
+        if (inst.type == 0) {
+            if (cache_idx != -1) {
+                handle_cache_hit(thread_num, cache_idx, inst);
+            } else {
+                int replacement_idx = thread_num % CACHE_SIZE;
+                printf("Thread %d: RD %d: %d (cache miss)\n", thread_num, inst.address, memory[inst.address]);
+                update_cache_line(cache, replacement_idx, inst.address, memory[inst.address], SHARED);
+            }
+        }
+        else if (inst.type == 1) {
+            if (cache_idx != -1) {
+                handle_cache_hit(thread_num, cache_idx, inst);
+            } else {
+                int replacement_idx = thread_num % CACHE_SIZE;
+                printf("Thread %d: WR %d: %d (cache miss)\n", thread_num, inst.address, inst.value);
+                
+                if (replacement_idx != -1 && cache[replacement_idx].state == SHARED) {
+                    omp_set_lock(&bus_locks[inst.address]);
+                    
+                    for (int i = 0; i < omp_get_num_procs(); i++) {
+                        if (i != thread_num) {
+                            int other_cache_idx = cache_lookup(caches[i], inst.address);
+                            if (other_cache_idx != -1) {
+                                caches[i][other_cache_idx].state = INVALID;
+                            }
+                        }
+                    }
+                    
+                    omp_unset_lock(&bus_locks[inst.address]);
                 }
-                break;
-            case 'W':
-                if (cache_idx != -1) {
-                    write_back_to_memory(cache_idx);
-                }
-                memory[addr] = val;
-                printf("Thread %d: WR %d: %d\n", thread_num, addr, val);
-                update_cache(thread_num % CACHE_SIZE, addr, val, 1);
-                break;
+                
+                update_cache_line(cache, replacement_idx, inst.address, inst.value, MODIFIED);
+                
+                memory[inst.address] = inst.value;
+            }
         }
     }
     fclose(inst_file);
 }
 
-
 int main(int argc, char *argv[]) {
-    initialize_memory();
-    initialize_cache();
-
-    int num_threads = argc > 1 ? atoi(argv[1]) : 1;
+    memset(memory, 0, sizeof(memory));
+    
+    int num_threads = (argc > 1) ? atoi(argv[1]) : 1;
     omp_set_num_threads(num_threads);
 
+    bus_locks = (omp_lock_t *)malloc(MEMORY_SIZE * sizeof(omp_lock_t));
+    for (int i = 0; i < MEMORY_SIZE; i++) {
+        omp_init_lock(&bus_locks[i]);
+    }
+    
+    caches = (cache_line **)malloc(num_threads * sizeof(cache_line *));
+    for (int i = 0; i < num_threads; i++) {
+        caches[i] = (cache_line *)malloc(CACHE_SIZE * sizeof(cache_line));
+        for (int j = 0; j < CACHE_SIZE; j++) {
+            caches[i][j].address = -1;
+            caches[i][j].value = 0;
+            caches[i][j].state = INVALID;
+        }
+    }
+    
     #pragma omp parallel
     {
         int thread_num = omp_get_thread_num();
         cpu_thread(thread_num);
     }
+
+    for (int i = 0; i < MEMORY_SIZE; i++) {
+        omp_destroy_lock(&bus_locks[i]);
+    }
+    free(bus_locks);
+
+    for (int i = 0; i < num_threads; i++) {
+        free(caches[i]);
+    }
+    free(caches);
 
     return 0;
 }
